03924fe633e50a84be5e9ac4a2ae1b27afe3693a
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-30 01:14:07 +0900

    :package:실행 해봐야함

--- XOR_encrypt.c
+++ XOR_encrypt.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-char encrypt(char enc);
+void encrypt(char input[], char key[]);
 
-int main(void)
-{
+int main(void) {
   char str[100];
-  printf("암호화할 문자를 입력해주세요.");
+  char my_key[] = "mysecret";
+  printf("암호화할 문자를 입력해주세요:");
   fgets(str, sizeof(str), stdin);
   str[strcspn(str, "\n")] = '\0';
-  for(int i = 0; str[i] != '\0'; i++){
-    
-  }
+  encrypt(str, my_key);
+  printf("암호화된 결과: %s\n", str);
+  return 0;
 }
 
+void encrypt(char input[], char key[]) {
+  int key_length = strlen(key);
+  for (int i = 0; i < strlen(input); i++) {
+    input[i] = input[i] ^ key[i % key_length];
+  }
+}
